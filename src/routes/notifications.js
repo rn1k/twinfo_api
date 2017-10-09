cef5a4971a31b796bed33d394b5cf8af8337d7c7
rn1k <maki_r12@yahoo.co.jp>
2017-10-10 00:55:08 +0900

    データ整形

--- src/routes/notifications.js
+++ src/routes/notifications.js
@@ -5,17 +5,37 @@ var path = require("path");
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database( process.env["HOME"] + '/.twinfo/data.db');
 
+function rows2notif(rows){
+  var notifs = {};
+  for (var row of rows){
+    id = row['trg_id'];
+    user = {
+        'src_uid': row['src_uid'],
+        'time': row['event_time']  
+      };
+    if ( id in notifs ){
+      notifs[id]['users'].push(user);
+    } else {
+      notifs[id] = {
+          'text': row['twit'],
+          'time': row['post_time'],
+          'users': [user]  
+        };
+    }
+  }
+  return notifs;
+}
 
 router.get('/', function(req, res, next) {
   res.header('Content-Type', 'application/json; charset=utf-8')
-  db.all("SELECT * FROM m1k", function(err, rows){
+  db.all('SELECT * FROM m1k', function(err, rows){
     if (!err) {
-      res.send(rows);
+      var notifs = rows2notif(rows);
+      res.send(notifs);
     } else {
       res.send(err);
     }
   });
-  db.close();
 });
 
  
@@ -25,7 +45,8 @@ router.get('/:user', function(req, res, next) {
 
   db.all(`SELECT * FROM ${req.params.user}`, function(err, rows){
     if (!err) {
-      res.send(rows);
+      var notifs = rows2notif(rows);
+      res.send(notifs);
     } else {
       res.send(err);
     } 
